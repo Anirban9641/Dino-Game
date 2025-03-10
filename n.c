50ffeebdfdb5137f2ffb6a41e5e65a6a8dcdd886
Anirban Paul <108251055+Anirban9641@users.noreply.github.com>
2025-03-11 02:50:36 +0530

    Add files via upload

---
+++ n.c
@@ -0,0 +1,15 @@
+    #include <stdio.h>
+
+int main() {
+    int intValue = 10;   
+    float floatValue = 5.75; 
+
+    
+    float result = intValue + floatValue;
+
+    
+    printf("%.2F\n", result);
+
+    return 0;
+}
+
